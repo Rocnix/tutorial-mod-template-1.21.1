6af151bb88614f4dfb101962d5a8d58ec780883f
Rocnix <55219363+Rocnix@users.noreply.github.com>
2024-11-27 03:34:12 -0800

    recipes and loot tables

--- src/main/java/net/rocnix/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/rocnix/tutorialmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.rocnix.tutorialmod.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.rocnix.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -25,6 +27,19 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create()
                     .strength(3f)
                     .requiresTool()
+            ));
+
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.create()
+                    .strength(3f)
+                    .requiresTool()
+            ));
+
+    public static final Block DEEPSLATE_PINK_GARNET_ORE = registerBlock("deepslate_pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.DEEPSLATE)
             ));
 
     private static Block registerBlock(String name, Block block) {
@@ -43,6 +58,8 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.DEEPSLATE_PINK_GARNET_ORE);
         });
     }
 
